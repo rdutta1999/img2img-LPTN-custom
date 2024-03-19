94cefb406d85204fbdfff594754d2d7df82fec22
Kumar <jha.kumarv@gmail.com>
2024-03-19 17:05:32 -0400

    Added inital commit for LPTN network low frequencies

--- src/LPTN_Network.py
+++ src/LPTN_Network.py
@@ -3,15 +3,58 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-
+from torch.nn import functional as F
+from Utils import laplacian_pyramid, reconstruct_image
+import cv2
+import numpy as np
+class ResidualBlock(nn.Module):
+    def __init__(self, features):
+        super().__init__()
+        self.layers=nn.Sequential(
+            nn.Conv2d(features, features, kernel_size=3, stride=1,padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(features, features, kernel_size=3, stride=1,padding=1),
+            nn.LeakyReLU()            
+        )
+    def forward(self,x):
+        return x+self.layers(x)
 class LPTN_Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-    )    
+        self.construct_pyramid=laplacian_pyramid
+        self.reconstruct_image=reconstruct_image
+        self.depth=3
+        self.low_frequency_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16,kernel_size=3, stride=1,padding=1),
+            nn.InstanceNorm2d(16),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=16, out_channels=64,kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            nn.Conv2d(in_channels=64, out_channels=16,kernel_size=3, stride=1,padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=16, out_channels=3,kernel_size=3, stride=1, padding=1),
+    )
+    def forward(self, x):
+        pyramid=laplacian_pyramid(x,self.depth)
+        low_freq_component=pyramid[-1]
+        low_freq_output=self.low_frequency_layers(low_freq_component)
+        low_freq_output=low_freq_output+low_freq_component
+        return F.tanh(low_freq_output)
+            
+net=LPTN_Network()
+
+img=cv2.imread("image.png")
+    
+image=img
+
+image=np.float32(np.transpose(image,(2,0,1)))
+inp=torch.tensor(np.array([image]))
+# inp=torch.rand((1,3,100,300))
+print(net(inp))
+
--- src/Utils.py
+++ src/Utils.py
@@ -15,61 +15,88 @@ def gaussian_pyramid(frame, height):
 
 # def upsample_image(img, scale):
     
-def laplacian_pyramid_torch(img, height):
-    laplacian_pyr=[]
-    current=img.clone()
-    gaussian_kernal=(1/273) * torch.Tensor([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4], [1,4,7,4,1]]).repeat(current.shape[1],1,1,1)
-    for i in range(height):        
+# def laplacian_pyramid_torch(img, height):
+#     laplacian_pyr=[]
+#     current=img.clone()
+#     gaussian_kernal=(1/273) * torch.Tensor([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4], [1,4,7,4,1]]).repeat(current.shape[1],1,1,1)
+#     for i in range(height):        
         
-        filtered= F.conv2d(current,gaussian_kernal, padding=2, groups=current.shape[1])
-        downsampled= filtered[:,:,::2,::2]
-        output = F.upsample(downsampled, scale_factor=2, mode='bilinear')
-        if(output.shape[2]!=current.shape[2]):
-            output=output[:,:,0:-1,:]
-        if(output.shape[3]!=current.shape[3]):
-            output=output[:,:,:,0:-1]
+#         filtered= F.conv2d(current,gaussian_kernal, padding=2, groups=current.shape[1])
+#         downsampled= filtered[:,:,::2,::2]
+#         output = F.upsample(downsampled, scale_factor=2, mode='bilinear')
+#         if(output.shape[2]!=current.shape[2]):
+#             output=output[:,:,0:-1,:]
+#         if(output.shape[3]!=current.shape[3]):
+#             output=output[:,:,:,0:-1]
         
-        print(current.shape)
-        print(downsampled.shape)
-        print(output.shape)
-        diff= current-output
-        laplacian_pyr.append(diff)
-        current=downsampled
-    return laplacian_pyr    
-
-def laplacian_pyramid_torch_2(img, levels):
+#         print(current.shape)
+#         print(downsampled.shape)
+#         print(output.shape)
+#         diff= current-output
+#         laplacian_pyr.append(diff)
+#         current=downsampled
+#     return laplacian_pyr    
+def laplacian_pyramid(img, levels):
     pyramid = []
-    current_level = img
+    
+    current_level = img.clone()
+    
+    gaussian_kernal=(1/273) * torch.Tensor([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4], [1,4,7,4,1]]).repeat(current_level.shape[1],1,1,1)
+    
     for i in range(levels - 1):
-        down = F.avg_pool2d(current_level, kernel_size=2, stride=2)
-        up = F.interpolate(down, scale_factor=2, mode='bilinear', align_corners=False)
+        filtered= F.conv2d(current_level,gaussian_kernal, padding=2, groups=current_level.shape[1])
+        # down = F.avg_pool2d(filtered, kernel_size=2, stride=2)
+        down = filtered[:,:,::2,::2]
+        
+        up = F.interpolate(down, scale_factor=2, mode='bilinear', align_corners=True)
+        if up.shape[2] != current_level.shape[2] or up.shape[3] != current_level.shape[3]:
+            up = F.interpolate(up, size=(current_level.shape[2], current_level.shape[3]))        
         residual = current_level - up
         pyramid.append(residual)
         current_level = down
     pyramid.append(current_level)
     return pyramid
 
-def laplacian_pyramid(gpA,height):
-    laplacian_pyr = [gpA[-1]]
-    for i in range(height,0,-1):
-        size = (gpA[i - 1].shape[1], gpA[i - 1].shape[0])
-        gaussian_expanded = cv2.pyrUp(gpA[i], dstsize=size)
-        laplacian = cv2.subtract(gpA[i-1],gaussian_expanded)
-        laplacian_pyr.append(laplacian)
-        cv2.imwrite(f"image{i}.png", laplacian)   
-    return laplacian_pyr
+# def laplacian_pyramid(gpA,height):
+#     laplacian_pyr = [gpA[-1]]
+#     for i in range(height,0,-1):
+#         size = (gpA[i - 1].shape[1], gpA[i - 1].shape[0])
+#         gaussian_expanded = cv2.pyrUp(gpA[i], dstsize=size)
+#         laplacian = cv2.subtract(gpA[i-1],gaussian_expanded)
+#         laplacian_pyr.append(laplacian)
+#         cv2.imwrite(f"image{i}.png", laplacian)   
+#     return laplacian_pyr
 
-def reconstruct_image(lbImage, height):
-    ls_ = lbImage[height-1]     
-    for j in range(height-2,0,-1):
-        size = (lbImage[j].shape[0], lbImage[j].shape[1])
-        print(size)
-        print(lbImage[j].shape)
-        print(ls_.shape)
-        ls_ = cv2.pyrUp(ls_)
-        ls_ = cv2.add(ls_,lbImage[j])
-    # ls_ = cv2.cvtColor(ls_, cv2.COLOR_YCR_CB2BGR)                    
-    return ls_
+def reconstruct_image(lpA,height):
+    img=lpA[-1]
+    for j in range(height-2,-1,-1):
+        img=F.interpolate(img, scale_factor=2, mode='bilinear', align_corners=True)
+        if img.shape[2] != lpA[j].shape[2] or img.shape[3] != lpA[j].shape[3]:
+            img = F.interpolate(img, size=(lpA[j].shape[2], lpA[j].shape[3]))        
+        
+        img=lpA[j]+img
+    return img
+        
+        
+# def reconstruct_image(lbImage, height):
+#     ls_ = lbImage[height-1]     
+#     for j in range(height-2,0,-1):
+#         size = (lbImage[j].shape[0], lbImage[j].shape[1])
+#         ls_ = cv2.pyrUp(ls_)
+#         ls_ = cv2.add(ls_,lbImage[j])
+#     # ls_ = cv2.cvtColor(ls_, cv2.COLOR_YCR_CB2BGR)                    
+#     return ls_
+
+# def reconstruct_image_torch(lpA,height):
+#     ls_ = lbImage[height-1]     
+#     for j in range(height-2,0,-1):
+#         size = (lbImage[j].shape[0], lbImage[j].shape[1])
+#         print(size)
+#         print(lbImage[j].shape)
+#         print(ls_.shape)
+#         ls_ = cv2.pyrUp(ls_)
+#         ls_ = cv2.add(ls_,lbImage[j])
+    
 def display_images(images, titles=None, cols=3, figsize=(15, 10)):
     rows = len(images) // cols + (0 if len(images) % cols == 0 else 1)
     fig, axes = plt.subplots(rows, cols, figsize=figsize)
@@ -88,7 +115,9 @@ def display_images(images, titles=None, cols=3, figsize=(15, 10)):
     plt.savefig("Pyramid.png")
 
 img=cv2.imread("image.png")
-image=img[0:-1,:,:]
+    
+image=img
+
 image=np.float32(np.transpose(image,(2,0,1)))
 inputs=torch.tensor(np.array([image]))
 # frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -97,19 +126,17 @@ inputs=torch.tensor(np.array([image]))
 # inputs=torch.randn(2,3,256,256)* 255
 # inputs.cuda()
 # print(inputs.shape)
-lpA=laplacian_pyramid_torch_2(inputs,3)
-lpaB=laplacian_pyramid(gaussian_pyramid(img,3),3)
-print(lpA)
-print(lpaB)
-
-lpA=[l.numpy().transpose(2,3,1,0).squeeze() for l in lpA]
+lpA=laplacian_pyramid(inputs,5)
+# lpA=[l.numpy().transpose(2,3,1,0).squeeze() for l in lpA]
 # print([l for l in lpA])
 # # gpA=gaussian_pyramid(frame,height)
 # # lpA=laplacian_pyramid(gpA, height)
 
-img= reconstruct_image(lpA, 3)
+img2= reconstruct_image(lpA, 5)
 
-cv2.imwrite("torch_reconstruction.png",img)
+img2=img2.numpy().transpose(2,3,1,0).squeeze()
+print(img.shape)
+cv2.imwrite("torch_reconstruction.png",img2)
 
 # cv2.imwrite("Pyramid_2.jpeg", img)
 # display_images(lpA)
