a663e8e8d09186e4443b7d25ed33b2a57b1fc4c5
Kumar <jha.kumarv@gmail.com>
2024-03-21 09:40:30 -0400

    Added some documentation

--- src/LPTN_Network.py
+++ src/LPTN_Network.py
@@ -7,6 +7,8 @@ from torch.nn import functional as F
 from Utils import laplacian_pyramid, reconstruct_image
 import cv2
 import numpy as np
+
+#Define residual block as stated in paper, residual block has two conv layers with leaky relus in between and allows for the input to factor into the output
 class ResidualBlock(nn.Module):
     def __init__(self, features):
         super().__init__()
@@ -16,15 +18,20 @@ class ResidualBlock(nn.Module):
             nn.Conv2d(features, features, kernel_size=3, stride=1,padding=1),
             nn.LeakyReLU()            
         )
+    # Pass through layers and add input to the output
     def forward(self,x):
         return x+self.layers(x)
+#Module for network
 class LPTN_Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
+        # Assign the pyramid construction method and the reconstruction method to the object
         self.construct_pyramid=laplacian_pyramid
         self.reconstruct_image=reconstruct_image
+        # 4 layers in pyramid (including last layer)
         self.depth=4
+        # The deepest layers in the network corresponding to the lower frequencies
         self.low_frequency_layers = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=16,kernel_size=3, stride=1,padding=1),
             nn.InstanceNorm2d(16),
@@ -40,6 +47,7 @@ class LPTN_Network(nn.Module):
             nn.LeakyReLU(),
             nn.Conv2d(in_channels=16, out_channels=3,kernel_size=3, stride=1, padding=1),
     )
+        # High frequency layers (as defined in the paper, should probably have a better name)
         self.high_frequency_layers=nn.Sequential(
             nn.Conv2d(in_channels=9, out_channels=16,kernel_size=3, stride=1,padding=1),
             nn.LeakyReLU(),
@@ -48,11 +56,13 @@ class LPTN_Network(nn.Module):
             ResidualBlock(16),
             nn.Conv2d(in_channels=16, out_channels=1,kernel_size=3, stride=1,padding=1),
         )
+        # Shallow layers for second highest frequencies (maybe a better name here?)
         self.other_freq_1_layers=nn.Sequential(
             nn.Conv2d(in_channels=1,out_channels=16, kernel_size=3,stride=1, padding=1),
             nn.LeakyReLU(),
             nn.Conv2d(in_channels=16,out_channels=1, kernel_size=3,stride=1, padding=1),
         )
+        # Shallow layers for highest frequencies
         self.other_freq_2_layers=nn.Sequential(
             nn.Conv2d(in_channels=1,out_channels=16, kernel_size=3,stride=1, padding=1),
             nn.LeakyReLU(),
@@ -60,16 +70,21 @@ class LPTN_Network(nn.Module):
         )
         
     def forward(self, x):
+        #Construct pyramid from input, x is a tensor of dimensions (N, C, W, H), typically C will be 3 as we're working with RGB images
         pyramid=laplacian_pyramid(x,self.depth) 
+        # Extract bottom layer of the pyramid
         low_freq_component=pyramid[-1]
+        # Pass through the low frequency layers
         low_freq_output=self.low_frequency_layers(low_freq_component)
+        # Following the paper closely here, variable names could use some work as earlier
         low_freq_output=low_freq_output+low_freq_component
         low_freq_output=F.tanh(low_freq_output)
+        # High frequency component
         high_freq_component=pyramid[-2]
+        # Using interpolate from torch to upsample, not sure if that is the best method?
         low_freq_component_upsampled=F.interpolate(low_freq_component, size=(high_freq_component.shape[2],high_freq_component.shape[3]), mode="bilinear")
         low_freq_output_upsampled=F.interpolate(low_freq_output, size=(high_freq_component.shape[2],high_freq_component.shape[3]), mode="bilinear")
         high_freq_input= torch.concat((high_freq_component,low_freq_component_upsampled,low_freq_output_upsampled), dim=1)
-        # print(high_freq_input.shape)
         mask= self.high_frequency_layers(high_freq_input)
         high_freq_output=high_freq_component*mask
         other_freq_component_1=pyramid[-3]
@@ -80,19 +95,23 @@ class LPTN_Network(nn.Module):
         mask_upsampled_2=F.interpolate(mask_upsampled_output, size=(other_freq_component_2.shape[2], other_freq_component_2.shape[3]), mode="bilinear")
         mask_upsampled_2_output= self.other_freq_2_layers(mask_upsampled_2)
         other_freq_component_2_output=mask_upsampled_2_output*other_freq_component_2
-        
+        # Return pyramid of components in order from largest to smallest to allow for reconstruction, should we return reconstruction as well? Depends on loss implementation
         return [other_freq_component_2_output, other_freq_component_1_output, high_freq_output, low_freq_output]
-net=LPTN_Network()
-
-img=cv2.imread("image.png")
     
-image=img
+# Small test code given you have an image named image.png in the directory
+    
+    
+# net=LPTN_Network()
 
-image=np.float32(np.transpose(image,(2,0,1)))
-inp=torch.tensor(np.array([image]))
-# inp=torch.rand((1,3,100,300))
-translated_pyr=net(inp)
+# img=cv2.imread("image.png")
+    
+# image=img
 
-img=reconstruct_image(translated_pyr, net.depth)
+# image=np.float32(np.transpose(image,(2,0,1)))
+# inp=torch.tensor(np.array([image]))
+# # inp=torch.rand((1,3,100,300))
+# translated_pyr=net(inp)
 
-cv2.imwrite("LPTN_Output.png", img.detach().numpy().transpose(2,3,1,0).squeeze())
+# img=reconstruct_image(translated_pyr, net.depth)
+
+# cv2.imwrite("LPTN_Output.png", img.detach().numpy().transpose(2,3,1,0).squeeze())
