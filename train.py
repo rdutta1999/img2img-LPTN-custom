fd2fc84c7284b3320590ab5fa67365b263459083
rdutta1999 <rdutta.1999@outlook.com>
2024-03-25 21:41:54 -0400

    added more deets to train script

--- train.py
+++ train.py
@@ -20,6 +20,8 @@ from torch.autograd import Variable
 from albumentations.pytorch import ToTensorV2
 from torch.utils.data import Dataset, DataLoader
 
+from LPTN_Network import LPTN_Network
+
 def resize_to_inputsz(x, **kwargs):
     
     # For RGB images with 3 channels:
@@ -150,6 +152,85 @@ class CustomDataset(Dataset):
 
     def __len__(self):
         return self.n_imgs
+    
+def train(train_loader, model, criterion, optimizer, scheduler, epoch, beta, use_weighted_loss_train):
+    model.train()
+    stream = tqdm(train_loader)
+    
+    for i, (images, targets) in enumerate(stream, start=1):        
+        images = images.to(DEVICE, non_blocking = True, dtype = torch.float)
+        targets = targets.to(DEVICE, non_blocking = True, dtype = torch.float)
+        
+        #optimizer.zero_grad(set_to_none = True)
+        optimizer.zero_grad()
+        
+        with torch.cuda.amp.autocast(enabled = use_amp):
+            outputs = model(images)  
+            loss = criterion(outputs, targets, includeBoundaryLoss = use_weighted_loss_train)
+     
+        if moving_loss['train']:
+            moving_loss['train'] = beta * moving_loss['train'] + (1-beta) * loss.item()
+        else:
+            moving_loss['train'] = loss.item()
+            
+        loss_values['train'].append(moving_loss['train'])
+        
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+
+        scale = scaler.get_scale()
+        scaler.update()
+        skip_lr_sched = (scale > scaler.get_scale())
+        
+        if not skip_lr_sched:
+            scheduler.step()      
+
+        stream.set_description(
+            "Epoch: {epoch}.  --Train--  Loss: {m_loss:04f}".format(epoch = epoch, m_loss = moving_loss['train'])
+        )
+
+def validate(val_loader, model, criterion, epoch, beta):
+    model.eval()
+    stream = tqdm(val_loader)
+    
+    with torch.no_grad():
+        for i, (images, targets) in enumerate(stream, start=1):
+            images = images.to(DEVICE, non_blocking=True, dtype = torch.float)
+            targets = targets.to(DEVICE, non_blocking=True, dtype = torch.float)
+            
+            with torch.cuda.amp.autocast(enabled = use_amp):
+                outputs = model(images)            
+                loss = criterion(outputs, targets)
+
+            if moving_loss['valid']:
+                moving_loss['valid'] = beta * moving_loss['valid'] + (1-beta) * loss.item()
+            else:
+                moving_loss['valid'] = loss.item()
+            
+            loss_values['valid'].append(moving_loss['valid'])
+            stream.set_description(
+                "Epoch: {epoch}.  --Valid--  Loss: {m_loss:04f}".format(epoch = epoch, m_loss = moving_loss['valid'])
+            )
+
+def train_and_validate(model, train_loader, val_loader, criterion, optimizer, scheduler, start_epoch, 
+                       n_epochs, ckpt_dir, save_freq, beta, use_weighted_loss_train):    
+    os.makedirs(ckpt_dir, exist_ok = True)
+    
+    for epoch in range(start_epoch + 1, start_epoch + n_epochs + 1):        
+        train(train_loader, model, criterion, optimizer, scheduler, epoch, beta, use_weighted_loss_train)
+        validate(val_loader, model, criterion, epoch, beta)
+        
+        ckpt_path = os.path.join(ckpt_dir, "{epoch}.pth".format(epoch = epoch))
+        
+        if epoch % save_freq == 0:
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                }, ckpt_path)
+        
+    return model
 
 def main():
     device = "cpu"
@@ -198,4 +279,49 @@ def main():
                             shuffle = False, 
                             num_workers = 0, 
                             pin_memory = True)
+
+
+    # ToDO  - Define the Loss Functions
+
+    # Define the Model
+    model = LPTN_Network()
+
+
+    # Training Params / HyperParams
+    start_epoch = 0
+    n_epochs = 15
+
+    learning_rate = 0.001
+
+    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, betas = (0.9, 0.999), eps = 1e-08, weight_decay = 0)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = learning_rate, 
+                                                    steps_per_epoch = len(train_loader), 
+                                                    epochs = n_epochs)
+
+    # DEFINE LOSS FUNC
+    #loss_fn = 
+    save_freq = 3
+    beta = 0.9
+    use_weighted_loss_train = True
+
+    use_amp = False
+    scaler = torch.cuda.amp.GradScaler(enabled = use_amp)
+
+    moving_loss = {'train': 0, 'valid': 0}
+    loss_values = {"train": [], "valid": []}
+
+    model.to(DEVICE)
+
+    model = train_and_validate(model, 
+                           train_loader, 
+                           val_loader,
+                           criterion = loss_fn, # ToDO
+                           optimizer = optimizer,
+                           scheduler = scheduler,
+                           start_epoch = start_epoch,
+                           n_epochs = n_epochs,                           
+                           ckpt_dir = CHECKPOINT_DIR,
+                           save_freq = save_freq,
+                           beta = beta,
+                           use_weighted_loss_train = use_weighted_loss_train)
     
