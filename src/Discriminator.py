4a6456f67afa0f0364491fe348e8fc2885a83968
rdutta1999 <rdutta.1999@outlook.com>
2024-03-31 22:52:38 -0400

    updated discriminator

--- src/Discriminator.py
+++ src/Discriminator.py
@@ -1,11 +1,12 @@
 from torch import nn as nn
 
-def discriminator_block(in_filters, out_filters, normalization=False):
-    layers = [nn.Conv2d(in_filters, out_filters, 3, stride=2, padding=1)]
-    layers.append(nn.LeakyReLU(0.2))
-    if normalization:
-        layers.append(nn.InstanceNorm2d(out_filters, affine=True))
-    return layers
+# def discriminator_block(in_filters, out_filters, normalization=False):
+
+#     layers = [nn.Conv2d(in_filters, out_filters, 3, stride=2, padding=1)]
+#     layers.append(nn.LeakyReLU(0.2))
+#     if normalization:
+#         layers.append(nn.InstanceNorm2d(out_filters, affine=True))
+#     return layers
 
 class Discriminator(nn.Module):
     def __init__(self):
@@ -16,10 +17,18 @@ class Discriminator(nn.Module):
             nn.Conv2d(3, 16, 3, stride=2, padding=1),
             nn.LeakyReLU(0.2),
             nn.InstanceNorm2d(16, affine=True),
-            *discriminator_block(16, 32),
-            *discriminator_block(32, 64),
-            *discriminator_block(64, 128),
-            *discriminator_block(128, 128),
+            # *discriminator_block(16, 32),
+            # *discriminator_block(32, 64),
+            # *discriminator_block(64, 128),
+            # *discriminator_block(128, 128),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1)
+            nn.LeakyReLU(0.2)
+            nn.Conv2d(32, 54, 3, stride=2, padding=1)
+            nn.LeakyReLU(0.2)
+            nn.Conv2d(64, 128, 3, stride=2, padding=1)
+            nn.LeakyReLU(0.2)
+            nn.Conv2d(128, 128, 3, stride=2, padding=1)
+            nn.LeakyReLU(0.2)
             nn.Conv2d(128, 1, 8, padding=0)
         )
 
